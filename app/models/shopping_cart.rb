161b6f5b4158f13712ec87b9827883d9c86160ee
Adele Weisz <adiel.weisz@gmail.com>
2022-03-24 19:42:00 -0500

    created shopping cart model and removed quanttiy and product id from orders

---
+++ app/models/shopping_cart.rb
@@ -0,0 +1,16 @@
+#Exercise: Decomposition
+# Write out all the steps necessary for your mini-capstone-api to allow users to purchase multiple products with a shopping cart. (aim for roughly 10 to 15 steps)
+
+
+
+
+#1 create a spreadsheet that clarifys the relationships betwween each step
+#  users, products, carted products and orders
+#2 walk through the process to make sure you accont fo reach step
+#3this includes selecting an assigned product/id and quantity and placing in cart
+#4creating a cart id number assigned to this cart an includes the specifics lik quantity and user id
+#5 include in this a table which can update the crt status ex removed purchased or carted 
+#6 create option to then place order- includes subtotal, tax and total user id and order id 
+
+# create shopping cart model 
+# in controller write a function to recieve products
