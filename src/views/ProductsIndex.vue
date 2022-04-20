42df134e5f147f02e081fdaf283f64b13471164f
Adele Weisz <adiel.weisz@gmail.com>
2022-04-20 18:48:56 -0500

    adding  heroku

---
+++ src/views/ProductsIndex.vue
@@ -0,0 +1,33 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      products: [],
+    };
+  },
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("/products").then((response) => {
+        console.log("products index", response);
+        this.products = response.data;
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="products-index">
+    <h1>All Products</h1>
+    <div v-for="product in products" v-bind:key="product.id">
+      <h2>{{ p.name }}</h2>
+      <img v-bind:src="photo.url" v-bind:alt="photo.name" />
+      <p>Width: {{ photo.width }}</p>
+      <p>Height: {{ photo.height }}</p>
+    </div>
+  </div>
+</template>
